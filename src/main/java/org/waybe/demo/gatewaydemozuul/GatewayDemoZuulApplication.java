d86ea517e4c96a936c3e143bcf3da84d08dc1861
Abraham <alarrazolo@palo-it.com>
2017-12-21 00:13:09 -0600

    Configuration for eureka-server

---
+++ src/main/java/org/waybe/demo/gatewaydemozuul/GatewayDemoZuulApplication.java
@@ -0,0 +1,16 @@
+package org.waybe.demo.gatewaydemozuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayDemoZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayDemoZuulApplication.class, args);
+	}
+}
